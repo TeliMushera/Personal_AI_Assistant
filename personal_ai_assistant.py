b33d139a57e0f725893c4ae8d6463c924acc2ca9
TELI MUSHERA <telimushera10@gmail.com>
2025-09-27 19:09:57 +0530

    Update personal_ai_assistant.py

--- personal_ai_assistant.py
+++ personal_ai_assistant.py
@@ -1,23 +1,21 @@
-!pip install transformers torch accelerate -q
-
 from transformers import pipeline
-assistant=pipeline('text2text-generation',model='google/flan-t5-large')
 
-"""building a chat function"""
+assistant = pipeline('text2text-generation', model='google/flan-t5-large')
 
-chat_history=[]
+chat_history = []
+
 def ask_ai(user_input):
-  global chat_history
-  chat_history.append(f'User: {user_input}')
-  context='\n'.join(chat_history[-5:])
-  response=assistant(context)[0]['generated_text']
-  chat_history.append(f'assistant:{response}')
-  return response
+    global chat_history
+    chat_history.append(f'User: {user_input}')
+    context = '\n'.join(chat_history[-5:])
+    response = assistant(context)[0]['generated_text']
+    chat_history.append(f'assistant: {response}')
+    return response
 
 while True:
-  user_input=input('You: ')
-  if user_input.lower() in ['exit','bye','quit']:
-    print('assistant goodbye!')
-    break
-  reply=ask_ai(user_input)
-  print(f'assisntant: {reply}')
+    user_input = input('You: ')
+    if user_input.lower() in ['exit', 'bye', 'quit']:
+        print('assistant goodbye!')
+        break
+    reply = ask_ai(user_input)
+    print(f'assistant: {reply}')
